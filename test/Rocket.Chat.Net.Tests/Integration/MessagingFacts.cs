1539eb7bd01cf5dd10036c9ea2008c1358e94f69
Mark Lopez <m@silvenga.com>
2016-03-23 15:34:05 -0700

    Fixes build.

---
+++ test/Rocket.Chat.Net.Tests/Integration/MessagingFacts.cs
@@ -0,0 +1,34 @@
+﻿namespace Rocket.Chat.Net.Tests.Integration
+{
+    using System.Threading.Tasks;
+
+    using Ploeh.AutoFixture;
+
+    using Rocket.Chat.Net.Tests.Helpers;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    [Trait("Category", "Driver")]
+    [Collection("Driver")]
+    public class MessagingFacts : DriverFactsBase
+    {
+        public MessagingFacts(ITestOutputHelper helper) : base(helper)
+        {
+        }
+
+        [Fact]
+        public async Task Can_send_messages()
+        {
+            var message = AutoFixture.Create<string>();
+            var roomId = "GENEREL";
+
+            // Act
+            await RocketChatDriver.ConnectAsync();
+            await RocketChatDriver.LoginWithEmailAsync(Constants.Email, Constants.Password);
+            var response = await RocketChatDriver.SendMessageAsync(message, roomId);
+
+            // Assert
+        }
+    }
+}
