ec858fe0613b7ca8f11738a13ed0fa98ef6f3478
Mark Lopez <m@silvenga.com>
2016-05-09 22:29:30 -0500

    More testing.

--- test.ps1
+++ test.ps1
@@ -7,5 +7,7 @@ $coveralls = (Resolve-Path "packages/coveralls.net.*/tools/csmacnz.coveralls.exe
 $targetargs = "`"$testDdl`" -noshadow -verbose -notrait category=Sandbox"
 $filter = "-filter:`"+[Rocket.Chat.Net]* -[Rocket.Chat.Net]Rocket.Chat.Net.Models.* -[*]JetBrains.*`""
 
+$env:os
+
 & $openCover -register:user -target:$xunit -targetargs:"$targetargs" -filter:"$filter" -output:$coverageXml
 & $coveralls --opencover -i $coverageXml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessa$env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB
