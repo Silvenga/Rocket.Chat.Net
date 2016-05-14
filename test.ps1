edfb66f34f450ac13dfa9a6d79d19acb00a3d4a1
Mark Lopez <m@silvenga.com>
2016-05-14 11:00:47 -0500

    Escape quotes.

--- test.ps1
+++ test.ps1
@@ -9,6 +9,6 @@ $testDdl2 = "test\Rocket.Chat.Net.Bot.Tests\bin\Debug\Rocket.Chat.Net.Bot.Tests.
 $targetargs = "`"$testDdl1`" `"$testDdl2`" -noshadow -notrait category=Sandbox "
 $filter = "+[Rocket.Chat.Net]Rocket.Chat.Net.* +[Rocket.Chat.Bot]Rocket.Chat.Net.Bot.*"
 
-& $openCover -register:user -target:$xunit -targetargs:"$targetargs" -filter:"$filter" -mergebyhash -output:$coverageXml
+& $openCover -register:user -target:$xunit -targetargs:`"$targetargs`" -filter:`"$filter`" -mergebyhash -output:$coverageXml
 
 & $coveralls --opencover -i $coverageXml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
